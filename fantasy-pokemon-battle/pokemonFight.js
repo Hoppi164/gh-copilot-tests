c7de13511c08e864e54022c76abee747fd7807d0
Andrew Hopkins <Hoppi164@gmail.com>
2022-01-30 20:17:56 +1000

    Made random pokemon work by choosing random index

--- fantasy-pokemon-battle/pokemonFight.js
+++ fantasy-pokemon-battle/pokemonFight.js
@@ -1,18 +1,26 @@
 // fetch the top 10 pokemon from the API and returns them in an array
+// Note: this function is no longer used, but it is kept here for reference
 function getPokemon() {
     return fetch("https://pokeapi.co/api/v2/pokemon?limit=10")
         .then(response => response.json())
         .then(pokemon => pokemon.results);
 }
 
+// function to pick a random number between 0 and 1118 (the number of pokemon in the API)
+function getRandomNumber() {
+    return Math.floor(Math.random() * 1118);
+}
+
 // fetch a random pokemon from the API and returns it
 function getRandomPokemon() {
-    return fetch("https://pokeapi.co/api/v2/pokemon/random")
+    let randomNumber = getRandomNumber();
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
         .then(response => response.json())
         .then(pokemon => pokemon);
 }
 
 // fetch the pokemon's details from the API and returns them in an array
+// Note: this function is no longer used, but it is kept here for reference
 function getPokemonDetails(pokemon) {
     return fetch(pokemon.url)
         .then(response => response.json())
@@ -30,16 +38,24 @@ function getWinner(pokemon1, pokemon2) {
 }
 
 
+async function randomPokemonFight(){
+   
+    // let allPokemon = await getPokemon();
+    // console.log(allPokemon)
+    
+    let pokemon1 = await getRandomPokemon();
+    let pokemon2 = await getRandomPokemon();
+    console.log(pokemon1.name, 'vs', pokemon2.name);
+    
+    // console.log(pokemon1);
+    // console.log(pokemon2);
+    
+    // pokemon1 = await getPokemonDetails(pokemon1);
+    // pokemon2 = await getPokemonDetails(pokemon2);
+    
+    let winner = await getWinner(pokemon1, pokemon2);
+    console.log(winner.name, 'is the winner!');
+    
+}
 
-
-// let allPokemon = await getPokemon();
-
-let pokemon1 = await getRandomPokemon();
-let pokemon2 = await getRandomPokemon();
-console.log(pokemon1.name, 'vs', pokemon2.name);
-
-pokemon1 = await getPokemonDetails(pokemon1);
-pokemon2 = await getPokemonDetails(pokemon2);
-
-let winner = await getWinner(pokemon1, pokemon2);
-console.log(winner.name, 'is the winner!');
+randomPokemonFight();
